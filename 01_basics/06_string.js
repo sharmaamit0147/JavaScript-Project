a597cdb7fc23459f9c83203a17280355212c37f7
sharmaamit0147 <122308020+sharmaamit0147@users.noreply.github.com>
2024-06-14 17:56:49 +0530

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,35 @@
+const name = "amit"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('amit-hs-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt("2"));
+// console.log(gameName.indexOf("t"));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5,4)
+// console.log(anotherString);
+
+const newStringOne = "    amit    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://amit.com/amit%20sharma"
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("sunder"));
+
+console.log(gameName.split("-"))
