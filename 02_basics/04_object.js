88c42e94fc3c0629dd13f3bfaa443a9c3348649c
sharmaamit0147 <122308020+sharmaamit0147@users.noreply.github.com>
2024-06-17 14:35:00 +0530

    object and json api

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -51,10 +51,44 @@ const users  = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+//   ****************** Object de-structure and JSON API ******************
+
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "amit",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
