168a9244f91185388a2cfd9b0d75722c34077940
sharmaamit0147 <122308020+sharmaamit0147@users.noreply.github.com>
2024-06-17 21:55:24 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "Febuary":
+        console.log("Febuary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
