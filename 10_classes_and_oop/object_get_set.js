272127324423fedcba5277fc1b6899d9e1007062
sharmaamit0147 <122308020+sharmaamit0147@users.noreply.github.com>
2024-07-01 11:33:51 +0530

    object get & set

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,24 @@
+const User = {
+    _email: 'a@as.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    },
+
+    get password(){
+        return this._password.toUpperCase()
+    },
+
+    set password(value){
+        this._password = value
+    }
+
+}
+
+const tea = Object.create(User)
+console.log(tea.password);
